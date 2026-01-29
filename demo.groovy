10e6ca68dd620fbc74a8b066c15ee18d4502ec06
SanchitaH26 <sanchitaharish2005@gmail.com>
2026-01-30 00:44:17 +0530

    Refactor Jenkins pipeline to add two numbers
    Removed unnecessary stages and added a new stage to add two numbers, displaying the results.

--- demo.groovy
+++ demo.groovy
@@ -2,38 +2,18 @@ pipeline {
     agent any
 
     stages {
-
-        stage('Hello Stage') {
-            steps {
-                echo 'Hello from Jenkins Pipeline using Groovy'
-            }
-        }
-
-        stage('Groovy Logic') {
+        stage('Add Two Numbers') {
             steps {
                 script {
                     int a = 10
                     int b = 20
                     int sum = a + b
-                    echo "Sum of a and b is: ${sum}"
+
+                    echo "First number: ${a}"
+                    echo "Second number: ${b}"
+                    echo "Sum = ${sum}"
                 }
             }
-        }
-
-        stage('Environment Info') {
-            steps {
-                echo "Job Name: ${env.JOB_NAME}"
-                echo "Build Number: ${env.BUILD_NUMBER}"
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'Pipeline executed successfully'
-        }
-        failure {
-            echo 'Pipeline failed'
         }
     }
 }
